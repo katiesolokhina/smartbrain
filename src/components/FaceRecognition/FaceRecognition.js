91915626e5c123dabe546ea8ce99b8e2508c0115
Katie Solokhina <katie.solokhina@gmail.com>
2021-09-07 18:16:12 -0400

    add face recognition

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './FaceRecognition.css'
+
+const FaceRecognition = ({box, imageUrl}) => {
+  return (
+    <div className="center">
+    <div className='absolute mt2'>
+      <img
+        id='inputImage'
+        alt="face detection"
+        src={imageUrl}
+        width='500px'
+        height='auto'
+      />
+      <div className='bounding-box' style={{top:box.topRow, right:box.rightCol, bottom:box.bottomRow, left: box.leftCol}}></div>
+    </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
